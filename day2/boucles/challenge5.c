b4f278b858d64c19a2c8aa6c2c33276050f5acc7
kaoutarmekn <kaoutarelmeknassi196@gmail.com>
2025-09-03 19:27:42 +0200

    challenge5.c

---
+++ day2/boucles/challenge5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int Base , Exposant , Resultat = 1;
+    printf("\n*** Calcul de la Puissance ***\n\n");
+    printf("La Base >> ");
+    scanf("%d",&Base);
+    printf("Exposant >> ");
+    scanf("%d",&Exposant);
+
+    for( int i = 1; i <= Exposant; i++)
+    {
+       Resultat = Resultat * Base;
+    }
+    printf("Le resultat >>> %d",Resultat);
+
+
+    return 0;
+}
