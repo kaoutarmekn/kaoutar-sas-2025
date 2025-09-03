4489a4bfbd5bdd5b2289b7d4da0f6bb241e3d249
kaoutarmekn <kaoutarelmeknassi196@gmail.com>
2025-09-03 15:32:10 +0200

    mini

---
+++ day2/Mini-prj/project.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main (){
+ 
+     float  A, B, C, D, E ; 
+     printf(" \n >> les cinq matiees princimales>> ; ") 
+    
+     printf("entrer la premier matiere :\n");
+     scanf("%f",&A); 
+
+          printf(" entrer la deuxieme matiere \n");
+               scanf("%f",&B); 
+
+          printf(" entrer la toixieme matiere \n");
+          scanf("%f", &C); 
+
+                    printf(" entrer la quatr matiere \n");
+                    scanf("%f", &D);
+
+                              printf(" entrer la sis matiere \n");
+                              scanf("%f", &E);  
+    
+
+ printf(" entrer la calcule automatiquement") ;
+ 
+       
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+    return 0 ;
+}
