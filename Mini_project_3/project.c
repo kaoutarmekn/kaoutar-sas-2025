be3046ce722c23301f103cac7d98f38ad8e0793b
kaoutarmekn <kaoutarelmeknassi196@gmail.com>
2025-09-10 10:40:27 +0200

    project.c

---
+++ Mini_project_3/project.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> 
+
+
+
+ int main(){
+    char mots_positifs[3][100] = {"hello" , "i am feeling happy"};
+char  mots_nigatifs[3][100] = {"hi" , "great to hear that" , "goodbaye"};
+char mots_neutre[3][100] = {"i am saad today" , "can y", "can y"};
+ char input; 
+  
+printf("** affichage de Chatbot\n\n");
+ while (1) {
+        printf("you : "); 
+        if(strstr(input, "happy") != NULL){
+            
+        }
+
+ }
+ printf("entrez input :");
+ scanf("%s" ,&input);
+
+
+
+
+return 0;    
+ }
+
+            
+
+
+
+
+
+
+
+
